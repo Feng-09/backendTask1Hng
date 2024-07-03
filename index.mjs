b92fd6f0c626cb3ebaf735fa678dd99b70fdc5c0
Boluwatife Oyewande <tifeng9@gmail.com>
2024-07-03 19:25:19 +0100

    bug fix

--- index.mjs
+++ index.mjs
@@ -2,10 +2,12 @@ import express from "express";
 import axios from 'axios';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import ipify from "ipify";
 
 const app = express()
 dotenv.config({path: './.env'})
 const apiKey = '7495ed6e5f124b00a2c182237240107';
+ipify.apiKey = process.env.IPIFY_API_KEY;
 
 app.set('trust proxy', true)
 app.use(cors())
@@ -16,8 +18,9 @@ app.get('/', (req, res) => {
 
 app.get('/api/hello', async (req, res) => {
     //fetch client ip address from ipify api
-    const ipResponse = await axios.get('https://api.ipify.org');
-    const clientIp = ipResponse.data
+    // const ipResponse = await axios.get('https://api.ipify.org');
+    const clientIp = await ipify({useIPv6: false});
+    // const clientIp = ipResponse.data
     //get client weather and city details using ip address
     const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${clientIp}`;
     const response = await axios.get(url)
